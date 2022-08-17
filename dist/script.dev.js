a1c4d7b2fe1ff64638b19d9c01d459cf57428565
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-08-17 13:55:09 +0100

    got buttons responding with js

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,4 +1,11 @@
 "use strict";
 
-var operators = ["+", "-", "*", "/"];
-var numberButtons = document.querySelectorAll([]);
+var display = document.getElementById('display');
+var buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map(function (button) {
+  button.addEventListener('click', function (e) {
+    if (e.target.innerText === 'C') {
+      display.innerText = '';
+    } else display.innerText += e.target.innerText;
+  });
+});
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const operators = ["+","-","*","/"];
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
 
-const numberButtons = document.querySelectorAll([])
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerText === 'C') {
+            display.innerText = '';
+        } else
+            display.innerText += e.target.innerText;
+        }
+    )});
+
