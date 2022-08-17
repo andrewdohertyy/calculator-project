cc3eb820e58c5986a5cd1738835b0f97f36d3638
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-08-17 18:36:50 +0100

    added plus and equals

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,11 +1,37 @@
 "use strict";
 
 var display = document.getElementById('display');
+var totalDisplay = document.getElementById('total-display');
 var buttons = Array.from(document.getElementsByClassName('button'));
+var plusButton = document.getElementById('plus');
+var equalsButton = document.getElementById('equals');
+var number = 0;
+var symbol = ""; //functions for handling events
+//DOM handling for click events
+
 buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
-    if (e.target.innerText === 'C') {
-      display.innerText = '';
-    } else display.innerText += e.target.innerText;
+    display.innerText += e.target.innerText;
+    console.log(number);
   });
+});
+plusButton.addEventListener('click', function (e) {
+  if (totalDisplay.innerText !== "") {
+    symbol = e.target.innerText;
+    display.innerText = "";
+  }
+
+  number = Number(display.innerText);
+  symbol = e.target.innerText;
+  display.innerText = "";
+  console.log(number); //console.log(number,typeof number);
+});
+equalsButton.addEventListener('click', function (e) {
+  if (symbol === "+") {
+    number += Number(display.innerText);
+    display.innerText = number;
+    console.log(number);
+  } //console.log(symbol);
+  //console.log(number,typeof number);
+
 });
--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 let display = document.getElementById('display');
+let totalDisplay = document.getElementById('total-display');
 let buttons = Array.from(document.getElementsByClassName('button'));
+let plusButton = document.getElementById('plus');
+let equalsButton = document.getElementById('equals');
+let number = 0
+let symbol = ""
 
-    buttons.forEach((button) =>  {
-        button.addEventListener('click', (e) => {
-            if (e.target.innerText === 'C') {
-                display.innerText = '';    
-            } else
-                display.innerText += e.target.innerText;
-            })});
+//functions for handling events
+
+//DOM handling for click events
+buttons.forEach((button) =>  {
+    button.addEventListener('click', (e) => {
+            display.innerText += e.target.innerText;
+            console.log(number);
+        })});
+
+plusButton.addEventListener('click', (e) => {
+    if (totalDisplay.innerText !== "") {
+        symbol = e.target.innerText
+        display.innerText = "";
+    }
+    number = Number(display.innerText);
+    symbol = e.target.innerText
+    display.innerText = "";
+    console.log(number);
+    //console.log(number,typeof number);
+});
+
+equalsButton.addEventListener('click', (e) => {
+    if (symbol === "+") {
+        number += Number(display.innerText)
+        display.innerText = number;
+        console.log(number);
+    }
+    //console.log(symbol);
+    //console.log(number,typeof number);
+});
